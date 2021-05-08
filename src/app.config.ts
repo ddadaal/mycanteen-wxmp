84c75102234ae52ae3c6016c645c2a0b8747f5b8
ddadaal <ddadaal@outlook.com>
2021-05-08 15:24:25 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+import { AppConfig } from "remax/wechat";
+
+const config: AppConfig = {
+  pages: ["pages/index/index"],
+  window: {
+    navigationBarTitleText: "校园大众点评",
+    navigationBarBackgroundColor: "#282c34",
+  },
+};
+
+export default config;
