4f504d5946b8fe93a6707108c97bda609ebf29bd
Chen Junda <ddadaal@outlook.com>
2021-05-09 23:51:40 +0800

    fix canteen texts

--- src/models/dish.ts
+++ src/models/dish.ts
@@ -10,11 +10,11 @@ export const FlavorTexts = {
 export const FlavorRanges = textObjectToArray(FlavorTexts, "key", "text");
 
 export const CanteenTexts = {
-  XueYi : "学一",
-  JiaYuan1: "家园1",
-  JiaYuan2: "家园2",
-  JiaYuan3: "家园3",
-  JiaYuan4: "家园4",
+  XueYi : "学一食堂",
+  JiaYuan1: "家园一层",
+  JiaYuan2: "家园二层",
+  JiaYuan3: "家园三层",
+  JiaYuan4: "家园四层",
   NongYuan1: "农园一层",
   NongYuan2: "农园二层",
   NongYuan3: "农园三层",
