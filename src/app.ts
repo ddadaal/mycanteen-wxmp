cd43c36d9388b62f2e05ad4848f08f7bf20c1877
ddadaal <ddadaal@outlook.com>
2021-05-09 08:09:56 +0800

    remove weui css

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import * as React from "react";
 import "./app.css";
-import "weui-miniprogram/miniprogram_dist/weui-wxss/dist/style/weui.wxss";
 import "annar/dist/annar.css";
 
 const App: React.FC = (props) => props.children as React.ReactElement;
